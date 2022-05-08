71fe0bb1cf5e254322f39d722aaa9b118b539cf2
Debolina <bhaumikdebolina@gmail.com>
2022-05-07 19:07:52 -0500

    ERD

---
+++ Employee_sql/QuickDBD-export.sql
@@ -0,0 +1,45 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Titles" (
+    "title_id" VARCHAR   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "Employees" (
+    "emp_no" VARCHAR   NOT NULL,
+    "emp_title" VARCHAR   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last-name" VARCHAR   NOT NULL,
+    "sex" VARCHAR   NOT NULL,
+    "hire-date" DATE   NOT NULL
+);
+
+CREATE TABLE "Department_manager" (
+    "dept_no" VARCHAR   NOT NULL,
+    "emp_no" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "Salaries" (
+    "emp_no" VARCHAR   NOT NULL,
+    "salary" integer   NOT NULL
+);
+
+CREATE TABLE "Department_employee" (
+    "emp_no" VARCHAR   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "Departments" (
+    "dept_no" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
